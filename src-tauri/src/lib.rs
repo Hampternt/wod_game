9727f8158d2c41bed1c1faeecb17e88b7b42c9e7
Hampternt <jesperlovland@proton.me>
2025-05-18 19:16:01 +0200

    adding dice rolling functionality.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -14,6 +14,35 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn dice_roller(amount_of_dice: &i32) -> Vec<DiceTemplate> {
+    let mut counter: i32 = 0;
+    let mut dice_rolled: Vec<DiceTemplate> = Vec::new();
+    loop {
+        dice_rolled.push(roll_dice());
+        counter += 1;
+        if counter >= *amount_of_dice {
+            break;
+        }
+    }
+
+    dice_rolled
+}
+
+fn roll_dice() -> DiceTemplate {
+    DiceTemplate {
+        dice_type: 3,
+        roll_number: 1,
+        successes: 1,
+    }
+}
+
+pub struct DiceTemplate {
+    dice_type: i32,
+    roll_number: i32,
+    successes: i32,
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
